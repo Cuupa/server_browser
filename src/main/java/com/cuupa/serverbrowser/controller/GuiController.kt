e0f98e2edf25187b96cbc5166714813ec93c6eeb
simon <simonthiel2@gmail.com>
2021-01-06 21:51:12 +0100

    made nullsafe

--- src/main/java/com/cuupa/serverbrowser/controller/GuiController.kt
+++ src/main/java/com/cuupa/serverbrowser/controller/GuiController.kt
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 
@@ -16,16 +15,16 @@ class GuiController {
     @Autowired
     private var fileCollector: FileCollector? = null
 
-    @GetMapping("/")
+    @GetMapping("")
     fun index(model: Model): String {
-        val list = fileCollector!!.collect("/")
+        val list = fileCollector?.collect("/") ?: listOf()
         model.addAttribute("gui", Gui("/", list))
         return "index"
     }
 
     @GetMapping("/list")
-    fun index1(@ModelAttribute("gui") gui: Gui, model: Model, @RequestParam("path") path: String): String {
-        val list = fileCollector?.collect(path)
+    fun index1(model: Model, @RequestParam("path") path: String): String {
+        val list = fileCollector?.collect(path) ?: listOf()
         model.addAttribute("gui", Gui(path, list))
         return "index"
     }
