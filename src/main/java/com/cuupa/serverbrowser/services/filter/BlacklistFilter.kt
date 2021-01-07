9eac7250ca26a2d516ba1cef8da24f59e4a4dd10
simon <simonthiel2@gmail.com>
2021-01-07 03:00:51 +0100

    added toString

--- src/main/java/com/cuupa/serverbrowser/services/filter/BlacklistFilter.kt
+++ src/main/java/com/cuupa/serverbrowser/services/filter/BlacklistFilter.kt
@@ -7,4 +7,8 @@ class BlacklistFilter(val blacklisted: List<String>) : BrowserFileFilter {
     override fun applies(file: File): Boolean {
         return blacklisted.any { file.absolutePath.startsWith(it) }.not()
     }
+
+    override fun toString(): String {
+        return "BlacklistFilter($blacklisted)"
+    }
 }
