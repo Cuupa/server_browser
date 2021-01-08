9f22636feae12d4b99d3e3533296fdc2cb6f7d3a
simon <simonthiel2@gmail.com>
2021-01-08 17:29:23 +0100

    removed unused dependencies. and from the commit before: changed start-path

--- src/main/java/com/cuupa/serverbrowser/controller/Gui.kt
+++ src/main/java/com/cuupa/serverbrowser/controller/Gui.kt
@@ -5,4 +5,8 @@ import com.cuupa.serverbrowser.services.BrowserFileObject
 class Gui(var currentDir: String?, var listings: List<BrowserFileObject>?, var downloadable: Boolean) {
 
     constructor() : this(null, listOf<BrowserFileObject>(), false)
+
+    companion object {
+        val name = "gui"
+    }
 }
